9a896546b7df317c0b596130318c5a9d2b7e53d4
Andreas Lau <andilau@users.noreply.github.com>
2022-09-20 13:01:28 +0200

    Remove outdated documentation (#64)
    * Remove outdated html documentation
    
    * Remove .gitignore
    
    * Set update filename angebote-openapi.yaml

--- regenrate-java-client.sh
+++ regenrate-java-client.sh
@@ -2,7 +2,7 @@
 
 rm -rf java-client
 
-swagger-codegen generate -i swagger.yaml -l java -o java-client \
+swagger-codegen generate -i angebote-openapi.yaml -l java -o java-client \
   --model-package de.europace.eli.model \
   --api-package de.europace.eli.api \
   --group-id de.europace \
